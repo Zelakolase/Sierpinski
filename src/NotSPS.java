8f025e4aa495b1934edc60f5243ecf3043e42ae9
Morad <460583232@sharkia1.moe.edu.eg>
2023-01-03 17:14:09 +0200

    Code fix

--- src/NotSPS.java
+++ src/NotSPS.java
@@ -3,10 +3,10 @@ import java.util.ArrayList;
 public class NotSPS {
     public static void main(String[] args) {
         for(int k = 1;k < Config.maxK; k += 2) {
-            ArrayList<Double> solutions = new ArrayList<>();
+            ArrayList<Integer> solutions = new ArrayList<>();
             for(int n = 0;n < Config.maxN; n++) {
                 double number = k * Utils.TwoPow(n) + 1;
-                if(Utils.isPrime(number)) solutions.add(number);
+                if(Utils.isPrime(number)) solutions.add(n);
             }
             System.out.println("K: "+k+" N: "+solutions);
         }
